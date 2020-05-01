d1a880413878e7452c68e03632f949686e666164
Eduardo Godoy <eduardo.ewgo@gmail.com>
2020-04-30 21:51:02 -0400

    Misplaced ENV variable.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /usr/src/app
 
 ENV PORT 8080
 ENV HOST 0.0.0.0
+ENV NODE_ENV production
 
 COPY package*.json ./
 
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const app = require('./src/config/express')();
 
 app.listen(port, () => {
-  console.log(`Live at ${port}`);
+  console.log(`Live at ${port} [${process.env.NODE_ENV}]`);
 });
 
 module.exports = app;
